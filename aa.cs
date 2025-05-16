e4883a51b72f485ffe56ea77c94d29ed3d098a13
Brad Brown <bradthomasbrown@outlook.com>
2025-05-16 09:36:23 -0500

    arbitrary random bit generator

--- aa.cs
+++ aa.cs
@@ -2,24 +2,55 @@ using System.Runtime.InteropServices;
 
 class _212a3cbcd7bb3b0147570e48665a1ee7_ {
 
- [DllImport("kernel32.dll", EntryPoint="VirtualAlloc"       )] static extern ulong a
-  (ulong a=0, ulong s=0, ulong d=0, ulong f=0 );
+ [StructLayout(LayoutKind.Explicit)]
+ public struct RandResult {
+  [FieldOffset(0x00)]public bool   Completed   ;
+  [FieldOffset(0x08)]public ushort BytesWritten;
+  [FieldOffset(0x10)]public byte[] Rand        ;
+  public static RandResult SatisfyingByteCount(uint byteCount) {
+   return new RandResult {
+    Completed    = false              ,
+    BytesWritten = 0                  ,
+    Rand         = new byte[byteCount]
+   };
+  }
+  public static RandResult SatisfyingBitCount(uint bitCount) {
+   ushort byteCount = (ushort)(_791400f31a7e37a2a6f91246d2b4a9bd_._6bf6a89cbe4e9246643cece419fbb1ac_((ulong)bitCount, (ulong)8)>>3);
+   return RandResult.SatisfyingByteCount(byteCount);
+  }
+ };
 
- [DllImport("kernel32.dll", EntryPoint="VirtualFree"        )] static extern ulong s
-  (ulong a=0, ulong s=0, ulong d=0            );
-
- [DllImport("ntdll.dll"   , EntryPoint="NtQueueApcThreadEx2")] static extern ulong d
-  (ulong a=0, ulong s=0, ulong d=0, ulong f=0,
-   ulong z=0, ulong x=0, ulong c=0            );
-
- unsafe public static byte[] RDRAND128() {
-  byte[] f = new byte[0x10];
-  ulong z = a(0,0x10000,0x3000,0x40);
-  *(ulong*) z    = 0x48018948f0c70f48;
-  *(ulong*)(z+8) = 0xc308418948f0c70f;
-  fixed (byte* x = f) unchecked { d((ulong)-2,0,1,z,(ulong)x); }
-  s(z,0x10000,0x8000);
-  return f;
+ unsafe public static RandResult Rand(uint bitCount) {
+  ulong mem = Kernel32.VirtualAlloc(protect:Kernel32.MemoryProtectionConstants.PAGE_EXECUTE_READWRITE);
+  int i = 0; foreach (byte b in new byte[] {
+   0x4c,       0x89, 0xc6,                // 00: mov rsi,r8
+   0x48,       0x89, 0xcd,                // 03: mov rbp,rcx
+               0x31, 0xc0,                // 06: xor eax,eax
+               0xfe, 0x02,                // 08: inc BYTE PTR [rdx]
+   0x48,       0xc1, 0xe9,       0x08,    // 0a: shr rcx,0x8
+               0xa8,             0x07,    // 0e: test al,0x7
+               0x75,             0x0b,    // 10: jnz 0x1a
+   0x48, 0x0f, 0xc7,       0xf1,          // 12: rdrand rcx
+         0x0f, 0x92,       0xc3,          // 16: setc bl
+               0x20, 0x1a,                // 19: and BYTE PTR [rdx],bl
+               0x74,             0x0c,    // 1b: jz 0x29
+               0x88, 0x0c, 0x06,          // 1d: mov BYTE PTR [rsi+rax*1],cl
+   0x48,       0xff, 0xc0,                // 20: inc rax
+   0x48,       0x83, 0xed,       0x08,    // 23: sub rbp,0x8
+               0x77,             0xe1,    // 27: ja 0xa
+   0x48,       0x89, 0x44, 0x22, 0x08,    // 29: mov QWORD PTR [rdx+riz*1+0x8],rax
+               0xc3                       // 2e: ret
+  }) { *(byte*)(mem+(ulong)i) = b; i++; }
+  RandResult res = RandResult.SatisfyingBitCount(bitCount);
+  fixed (byte* b = res.Rand)
+   Ntdll.NtQueueApcThreadEx2(
+    apcRoutine : mem,
+    arg0       : bitCount,
+    arg1       : (ulong)&res.Completed,
+    arg2       : (ulong)b
+   );
+  Kernel32.VirtualFree(mem);
+  return res;
  }
 
 }
